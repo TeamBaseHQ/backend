c7a5abfc25d81ebad60178f2308b070da5bd9d3c
Kunal Varma <kunalvarma05@gmail.com>
2017-10-16 01:44:30 +0530

    Setup Beanstalkd

--- config/queue.php
+++ config/queue.php
@@ -43,7 +43,7 @@ return [
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
-            'host' => 'localhost',
+            'host' => 'beanstalkd',
             'queue' => 'default',
             'retry_after' => 90,
         ],
