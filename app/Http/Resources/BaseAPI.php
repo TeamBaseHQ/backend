509fa5b4d0de167004f953e77086a5e2fff5beed
Kunal Varma <kunalvarma05@gmail.com>
2017-10-17 01:41:12 +0530

    Base API endpoint

---
+++ app/Http/Resources/BaseAPI.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class BaseAPI extends Resource
+{
+    public function __construct($resource = null)
+    {
+        parent::__construct($resource);
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "title" => config('app.name', "Base") . " API",
+            "version" => env('APP_VERSION', '1.0'),
+        ];
+    }
+}
