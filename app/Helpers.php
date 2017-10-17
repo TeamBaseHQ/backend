bb39c4c1c047f2a14f58a52720dfd52755830436
Kunal Varma <kunalvarma05@gmail.com>
2017-10-17 17:24:24 +0530

    requireAllFilesFromDirectory Helper function

---
+++ app/Helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Base;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+abstract class Helpers
+{
+    public static function requireAllFilesInDirectory(string $dir = "")
+    {
+        $directoryIterator = new RecursiveDirectoryIterator($dir);
+        $iterator = new RecursiveIteratorIterator($directoryIterator);
+
+        foreach ($iterator as $file) {
+            $fileName = $file->getFilename();
+            $filePath = $file->getRealPath();
+            $fileIsValid = !$file->isDir() && ($fileName !== "." || $fileName !== "..");
+
+            if ($fileIsValid) {
+                require_once $filePath;
+            }
+        }
+    }
+}
