658793fec132cb41788491caec75106502797368
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 02:05:20 +0530

    create invitations table migration

---
+++ database/migrations/2017_11_02_202848_create_invitations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('message');
+            $table->boolean('is_accepted')->default(false);
+            $table->unsignedInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitations');
+    }
+}
