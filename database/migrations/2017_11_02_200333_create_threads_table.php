2f0d1db14397b816741847a7c78249509bbad554
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 01:36:42 +0530

    create threads table migration

---
+++ database/migrations/2017_11_02_200333_create_threads_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->string('description')->nullable();
+            $table->string('slug')->unique();
+            $table->json('notification_meta')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('channel_id');
+            $table->foreign('channel_id')->references('id')->on('channels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
