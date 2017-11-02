06830e83b65e76a391541876198c1934321b3bb2
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 02:19:07 +0530

    Star model

---
+++ app/Star.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Base;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Star extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "stars";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'message_id', 'user_id'
+    ];
+}
