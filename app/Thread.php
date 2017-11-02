3630110ee92032228d14a045200d58eb4defe8fb
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 02:18:36 +0530

    Thread Model

---
+++ app/Thread.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Base;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "threads";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'channel_id', 'user_id', 'notification_meta'
+    ];
+}
