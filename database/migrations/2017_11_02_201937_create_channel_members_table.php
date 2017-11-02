612452a0d5383c24eeadb3114e2d4653d93057f2
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 01:53:06 +0530

    create channel members table migration

---
+++ database/migrations/2017_11_02_201937_create_channel_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChannelMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channel_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('last_viewed_at')->nullable();
+            $table->unsignedInteger('messages_viewed')->default(0);
+            $table->unsignedInteger('channel_id');
+            $table->foreign('channel_id')->references('id')->on('channels');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel_members');
+    }
+}
