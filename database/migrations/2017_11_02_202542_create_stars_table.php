d0344f250e7f8b418d4848a77d4e1a5e266b7e0a
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 01:57:55 +0530

    create stars table migration

---
+++ database/migrations/2017_11_02_202542_create_stars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('message_id');
+            $table->foreign('message_id')->references('id')->on('messages');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stars');
+    }
+}
