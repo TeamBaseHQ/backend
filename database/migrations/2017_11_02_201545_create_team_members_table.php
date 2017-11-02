cab32d91108bd1c25b7da984d28dbcdd24e7ff1e
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 01:49:25 +0530

    create team members table migration

---
+++ database/migrations/2017_11_02_201545_create_team_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_members');
+    }
+}
