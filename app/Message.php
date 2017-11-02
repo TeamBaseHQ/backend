2d82d5098cb83fb2bd3ed5e19d7101f5ae6f83cb
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 02:18:45 +0530

    Message model

---
+++ app/Message.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Base;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "messages";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content', 'type', 'thread_id', 'sender_type', 'sender_id', 'meta'
+    ];
+}
