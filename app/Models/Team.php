b5e0464555bc04626cda8ee83cc1dd884c8f3936
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 16:03:15 +0530

    Fix typo

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,9 +3,9 @@
 namespace Base\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
