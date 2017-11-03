7cfe5fc48b22ce8e22c8d4d08797dc7f406c1cae
Kunal Varma <kunalvarma05@gmail.com>
2017-11-04 01:14:09 +0530

    Install api-consumer

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Optimus\ApiConsumer\Provider\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
