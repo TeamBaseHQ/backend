802ca0b04ba576f2076eafb440028fb59d88390c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 13:18:02 +0530

    create preferences table migration

---
+++ database/migrations/2017_11_03_074423_create_preferences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('default_value')->nullable();
+            $table->string('data_type')->default('string');
+            $table->unsignedInteger('preference_category_id');
+            $table->foreign('preference_category_id')->references('id')->on('preference_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preferences');
+    }
+}
