4c0f30a5e1bd060639daf7255e52f7864ec43ea7
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 13:22:42 +0530

    PreferenceCategory model

---
+++ app/PreferenceCategory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Base;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PreferenceCategory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "preference_categories";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+}
