07e01559afd7791db370d19eed9b4787741c0cb7
Kunal Varma <kunalvarma05@gmail.com>
2017-11-03 13:34:28 +0530

    create custom_preferences table migration

---
+++ database/migrations/2017_11_03_080230_create_custom_preferences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomPreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_preferences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('value');
+            $table->morphs('owner');
+            $table->unsignedInteger('preference_id');
+            $table->foreign('preference_id')->references('id')->on('preferences');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_preferences');
+    }
+}
