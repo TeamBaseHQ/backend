21c7580dbb64463e4768c270c3b41bcc9fe9084c
Kunal Varma <kunalvarma05@gmail.com>
2017-11-04 23:50:52 +0530

    Move create-user route out of aut:api middleware group

--- routes/api/user.php
+++ routes/api/user.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix' => 'users'], function () {
+Route::group(['prefix' => 'users', 'namespace' => "User"], function () {
+    Route::post('/', ['as' => "create-user", 'uses' => "CreateUser"]);
     Route::post('login', ['as' => 'login', 'uses' => "LoginController@login"]);
     Route::post('login/refresh', ['as' => 'login-refresh', 'uses' => "LoginController@refresh"]);
 
-    Route::group(['middleware' => 'auth:api', 'namespace' => "User"], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::get('/{id}', ['as' => "show-user", 'uses' => "ShowUser"]);
-        Route::post('/', ['as' => "create-user", 'uses' => "CreateUser"]);
     });
 
 });
