9a1eba8f625ab1028504557ea750e0dad0adf689
Kunal Varma <kunalvarma05@gmail.com>
2017-11-04 23:53:13 +0530

    Fire UserWasCreated event

--- app/Http/Controllers/Api/User/CreateUser.php
+++ app/Http/Controllers/Api/User/CreateUser.php
@@ -2,6 +2,7 @@
 
 namespace Base\Http\Controllers\Api\User;
 
+use Base\Events\User\UserWasCreated;
 use Base\Http\Requests\CreateUserRequest;
 use Base\Mail\ConfirmUser;
 use Base\Models\User;
@@ -12,10 +13,13 @@ class CreateUser extends APIController
 {
     public function __invoke(CreateUserRequest $request)
     {
-        return new ConfirmUser(User::first());
+        // Fetch data from request
         $data = $request->only(['name', 'email', 'password']);
+        // Create user
         $user = User::create($data);
-
+        // Fire Event
+        event(new UserWasCreated($user));
+        // Return response
         return new UserResource($user);
     }
 }
