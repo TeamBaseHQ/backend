18b99718782d54d00e7a7f6693e0434f7993f944
Kunal Varma <kunalvarma05@gmail.com>
2017-11-04 18:04:36 +0530

    CreateUser Request

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Base\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => "bail|required|min:4|max:255",
+            'email' => "bail|required|email|unique:users",
+            'username' => "bail|required|alpha_dash|unique:users",
+            'password' => 'bail|required|confirmed',
+        ];
+    }
+}
