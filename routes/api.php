282b325acf19fbb82f6be929197d482c9fc30318
Kunal Varma <kunalvarma05@gmail.com>
2017-11-04 15:58:44 +0530

    Organize API Routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use Base\Helpers;
 use Base\Http\Resources\BaseAPI;
 
 Route::get('/', function () {
     return new BaseAPI();
 });
 
-Route::group(['prefix' => 'users'], function () {
-    Route::post('login', ['as' => 'login', 'uses' => "LoginController@login"]);
-    Route::post('login/refresh', ['as' => 'login-refresh', 'uses' => "LoginController@refresh"]);
-});
-
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
+// API Routes
+Helpers::requireAllFilesInDirectory(__DIR__ . "/api");
---
+++ routes/api/user.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::group(['prefix' => 'users'], function () {
+    Route::post('login', ['as' => 'login', 'uses' => "LoginController@login"]);
+    Route::post('login/refresh', ['as' => 'login-refresh', 'uses' => "LoginController@refresh"]);
+
+    Route::group(['middleware' => 'auth:api', 'namespace' => "User"], function () {
+        Route::get('/{id}', ['as' => "show-user", 'uses' => "ShowUser"]);
+    });
+
+});
