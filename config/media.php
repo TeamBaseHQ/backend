4f09cd442103d9f5ef8f796c854670e77b4a38be
Kunal Varma <kunalvarma05@gmail.com>
2017-11-05 15:53:21 +0530

    Config: Media Conversions

---
+++ config/media.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'conversions' => [
+        'display_picture' => [
+            'small' => [
+                'width' => 40,
+                'height' => 40,
+                'quality' => 70,
+            ],
+            'thumb' => [
+                'width' => 100,
+                'height' => 100,
+                'quality' => 70,
+            ],
+            'medium' => [
+                'width' => 160,
+                'height' => 160,
+                'quality' => 70,
+            ],
+            'large' => [
+                'width' => 220,
+                'height' => 220,
+                'quality' => 70,
+            ],
+        ]
+    ]
+];
