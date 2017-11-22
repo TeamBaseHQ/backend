f0d48fb266bebfd354327b9143a570b057349209
Kunal Varma <kunalvarma05@gmail.com>
2017-11-22 16:45:44 +0530

    Broadcast ChannelMemberWasRemoved in RemoveChannelMember controller

--- app/Http/Controllers/Api/Team/Channel/Member/RemoveChannelMember.php
+++ app/Http/Controllers/Api/Team/Channel/Member/RemoveChannelMember.php
@@ -3,8 +3,10 @@
 namespace Base\Http\Controllers\Api\Team\Channel\Member;
 
 use Base\Models\Team;
+use Base\Models\User;
 use Illuminate\Http\Request;
 use Base\Http\Controllers\Api\APIController;
+use Base\Events\Team\Channel\ChannelMemberWasRemoved;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class RemoveChannelMember extends APIController
@@ -26,10 +28,11 @@ class RemoveChannelMember extends APIController
         throw_if(!$channel, (new ModelNotFoundException())->setModel(Channel::class, $chSlug));
 
         $user_id = $id;
+        $userToRemove = User::findOrFail($user_id);
 
         // If the Channel is Private
         if ($channel->isPrivate()) {
-            $currentUserIsChannelOwner = $request->user()->id == $channel->user_id;
+            $currentUserIsChannelOwner = $request->user()->id === $channel->user_id;
             abort_unless($currentUserIsChannelOwner, 403, "You are not allowed to join or add members to this channel.");
         }
 
@@ -38,8 +41,10 @@ class RemoveChannelMember extends APIController
         // Already a member
         abort_unless($member, 500, "User is not a member of this channel.");
 
-        // Add User to the Channel
+        // Remove User from the Channel
         $channel->members()->detach($user_id);
+
+        broadcast(new ChannelMemberWasRemoved($userToRemove, $channel, $team));
 
         return response("");
     }
